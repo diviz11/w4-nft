7946970454830038573abe77fa10853b7bd0993d
Diviz Akkshat <d.akkshat@gmail.com>
2023-10-09 14:07:32 +0530

    part1 of assignment
    Signed-off-by: Diviz Akkshat <d.akkshat@gmail.com>

---
+++ scripts/mint-nft.js
@@ -0,0 +1,38 @@
+require('dotenv').config();
+const ethers = require('ethers');
+
+// Get Alchemy API Key
+const API_KEY = process.env.API_KEY;
+
+// Define an Alchemy Provider
+const provider = new ethers.providers.JsonRpcProvider(process.env.API_URL)
+
+const contract = require("../artifacts/contracts/MyNFT.sol/MyNFT.json");
+
+// Create a signer
+const privateKey = process.env.PRIVATE_KEY
+const signer = new ethers.Wallet(privateKey, provider)
+
+// Get contract ABI and address
+const abi = contract.abi
+const contractAddress = '0x12aA7390006cD66D6D976FE0c5D0AD764f4AAac7'
+
+// Create a contract instance
+const myNftContract = new ethers.Contract(contractAddress, abi, signer)
+
+// Get the NFT Metadata IPFS URL
+const tokenUri = "https://gateway.pinata.cloud/ipfs/QmUMvvixTQzKm8BrfsKMDTeGqkEoYkXhv5qx6akL7HVYRi"
+
+// Call mintNFT function
+const mintNFT = async () => {
+    let nftTxn = await myNftContract.mintNFT(signer.address, tokenUri)
+    await nftTxn.wait()
+    console.log(`NFT Minted! Check it out at: https://sepolia.etherscan.io/tx/${nftTxn.hash}`)
+}
+
+mintNFT()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error);
+        process.exit(1);
+    });
