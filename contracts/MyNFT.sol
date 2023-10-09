c4336112e5b5e9586ccfbcb0fb493d9dc9177fbf
Diviz Akkshat <d.akkshat@gmail.com>
2023-10-09 21:13:52 +0530

    updated contract and create-next-app
    Signed-off-by: Diviz Akkshat <d.akkshat@gmail.com>

--- contracts/MyNFT.sol
+++ contracts/MyNFT.sol
@@ -1,24 +1,40 @@
-// Contract based on https://docs.openzeppelin.com/contracts/4.x/erc721
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract MyNFT is ERC721URIStorage {
+contract MyNFT is ERC721URIStorage, Ownable {
     using Counters for Counters.Counter;
 
     Counters.Counter private _tokenIds;
+    uint256 public maxSupply;
+    mapping(uint256 => string) private _tokenImages;
 
-    constructor() ERC721("MyNFT", "MNFT") {}
+    constructor(uint256 _maxSupply) ERC721("MyNFT", "MNFT") {
+        maxSupply = _maxSupply;
+    }
 
-    function mintNFT(address recipient, string memory tokenURI) public returns (uint256) {
+    function mintNFT(address recipient, string memory tokenURI, string memory imageURI) public onlyOwner returns (uint256) {
+        require(_tokenIds.current() < maxSupply, "Max supply reached");
+        
         _tokenIds.increment();
-
         uint256 newItemId = _tokenIds.current();
         _mint(recipient, newItemId);
         _setTokenURI(newItemId, tokenURI);
+        _tokenImages[newItemId] = imageURI;
 
         return newItemId;
+    }
+
+    function changeImage(uint256 tokenId, string memory newImageURI) public onlyOwnerOf(tokenId) {
+        require(tokenId <= _tokenIds.current(), "Token ID does not exist");
+        _tokenImages[tokenId] = newImageURI;
+    }
+
+    function getTokenImage(uint256 tokenId) public view returns (string memory) {
+        require(tokenId <= _tokenIds.current(), "Token ID does not exist");
+        return _tokenImages[tokenId];
     }
 }
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,15 +1,18 @@
+const { ethers } = require("hardhat");
+
 async function main() {
-    // Grab the contract factory
-    const MyNFT = await ethers.getContractFactory("MyNFT");
- 
-    // Start deployment, returning a promise that resolves to a contract object
-    const myNFT = await MyNFT.deploy(); // Instance of the contract
-    console.log("Contract deployed to address:", myNFT.address);
- }
- 
- main()
-   .then(() => process.exit(0))
-   .catch(error => {
-     console.error(error);
-     process.exit(1);
-   });
+  const MyNFT = await ethers.getContractFactory("MyNFT");
+  const maxSupply = 3000; // Set your desired maxSupply here
+  const contract = await MyNFT.deploy(maxSupply);
+
+  await contract.deployed();
+
+  console.log("Contract deployed to:", contract.address);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
